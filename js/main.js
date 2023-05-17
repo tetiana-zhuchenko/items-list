a66154273a1ea48dca0ec283483571aaf12b9955
Tanya <taniyhamail@gmail.ua>
2023-05-17 16:09:21 +0300

    added search functionality

--- js/main.js
+++ js/main.js
@@ -1,20 +1,13 @@
 const itemContainer = document.querySelector('[data-items]');
 const newItemsForm = document.querySelector('[data-new-item-form]');
 const newItemInput = document.querySelector('[data-new-item-input]');
-
+const serchInput = document.querySelector('[data-search]');
+const searchInput = document.querySelector('[data-search-input]');
 const LOCAL_STORAGE_ITEM_KEY = 'task.itemsList';
 let itemsList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_ITEM_KEY)) || [];
 console.log(itemsList);
 
-itemContainer.addEventListener('click', (e) => {
-  if (e.target.tagName.toLowerCase() === 'button') {
-    itemsList = itemsList.filter(
-      (item) => item.name !== e.target.previousElementSibling.innerHTML
-    );
-  }
-  saveAndRender();
-});
-
+// add item
 newItemsForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
@@ -23,9 +16,9 @@ newItemsForm.addEventListener('submit', (e) => {
     alert('Please add the item');
     return;
   }
-  const Item = createListItem(itemName);
+  const item = createListItem(itemName);
   newItemInput.value = null;
-  itemsList.push(Item);
+  itemsList.push(item);
   saveAndRender();
 });
 
@@ -35,14 +28,14 @@ function createListItem(name) {
 
 function saveAndRender() {
   save();
-  render();
+  render(itemsList);
 }
 
 function save() {
   localStorage.setItem(LOCAL_STORAGE_ITEM_KEY, JSON.stringify(itemsList));
 }
 
-function render() {
+function render(itemsList) {
   clearElement(itemContainer);
 
   itemsList.forEach((item) => {
@@ -60,7 +53,46 @@ function clearElement(element) {
   }
 }
 
-render();
+render(itemsList);
+
+// delete item
+itemContainer.addEventListener('click', (e) => {
+  if (e.target.tagName.toLowerCase() === 'button') {
+    itemsList = itemsList.filter(
+      (item) => item.name !== e.target.previousElementSibling.innerHTML
+    );
+  }
+  saveAndRender();
+});
+
+// search item
+// listener for input
+serchInput.addEventListener('input', (e) => {
+  const searchValue = e.target.value;
+  console.log(searchValue);
+
+  const tempItemsList = filteredListForSearch(searchValue);
+
+  render(tempItemsList);
+});
+
+// listener for submit
+serchInput.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchValue = searchInput.value;
+  const tempItemsList = filteredListForSearch(searchValue);
+
+  render(tempItemsList);
+});
+
+function filteredListForSearch(value) {
+  return itemsList.filter((item) =>
+    item.name.toLowerCase().includes(value.toLowerCase())
+  );
+}
+
+// previus version, without locallStorage
 
 // const addItemform = document.querySelector('.input-items-form');
 // const newItemInput = document.querySelector('.new-item-input');
